0b070bd51c796be9f767f1845e96379b5367faca
Łukasz Włódarczyk <lukasz.wlodarczyk@docflow.pl>
2017-03-26 13:36:43 +0200

    status method added

---
+++ src/main/java/pi/domain/StatusDTO.java
@@ -0,0 +1,15 @@
+package pi.domain;
+
+public class StatusDTO {
+    
+    private String uptime;
+
+    public String getUptime() {
+        return uptime;
+    }
+
+    public void setUptime(String uptime) {
+        this.uptime = uptime;
+    }
+    
+}
---
+++ src/main/java/pi/rest/MainController.java
@@ -0,0 +1,57 @@
+package pi.rest;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import pi.domain.StatusDTO;
+
+@RestController
+@RequestMapping("/rest-api")
+public class MainController {
+    
+    @GetMapping("/status")
+    public @ResponseBody StatusDTO status() throws IOException, InterruptedException {
+        StatusDTO status = new StatusDTO();
+        
+        //Build command 
+        List<String> commands = new ArrayList<String>();
+        commands.add("uptime");
+
+        //Run macro on target
+        ProcessBuilder pb = new ProcessBuilder(commands);
+        pb.directory(new File("/home/pi"));
+        pb.redirectErrorStream(true);
+        Process process = pb.start();
+        
+        //Read output
+        StringBuilder out = new StringBuilder();
+        BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        String line = null, previous = null;
+        while ((line = br.readLine()) != null)
+            if (!line.equals(previous)) {
+                previous = line;
+                out.append(line).append('\n');
+                System.out.println(line);
+            }
+
+        //Check result
+        if (process.waitFor() == 0) {
+            System.out.println("Success!");
+            System.exit(0);
+        }
+        
+        System.out.println("commands: " + commands);
+        System.out.println("output: " + out);
+        
+        status.setUptime(out.toString());
+        
+        return status;
+    }
+}
