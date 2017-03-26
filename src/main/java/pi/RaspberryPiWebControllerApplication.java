8086ad93640a54b981f8ba339e47bb198fdb4f8e
Łukasz Włódarczyk <lukasz.wlodarczyk@docflow.pl>
2017-03-26 13:17:12 +0200

    project first draw

---
+++ src/main/java/pi/RaspberryPiWebControllerApplication.java
@@ -0,0 +1,12 @@
+package pi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaspberryPiWebControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaspberryPiWebControllerApplication.class, args);
+	}
+}
